eb0e86c879991917219fead01a1ae4245bd2a3eb
moonhx <m15802961385@163.com>
2019-08-12 20:22:32 +0800

    添加了画连续点的demo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,56 +10,92 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  
+  List<LatLng> _points;
+
+  @override
+  void initState() {
+    _points = <LatLng>[
+      LatLng(51.5, -0.09),
+      LatLng(53.3498, -6.2603),
+      LatLng(48.8566, 2.3522),
+    ];
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'map_demo',
-      home: Scaffold(
-        appBar: AppBar(title: Text('OnTap')),
-        body: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
-                child: Text('Try tapping on the markers'),
-              ),
-              Flexible(
-                child: FlutterMap(
-                  options: MapOptions(
-                    onTap: (lat){
-                      // print(lat);
-                    },
-                    onDragStart: (lat){
-                      print(lat);
-                      return true;
-                    },
-                    onDragUpdate: (lat){
-                      print(lat);
-                    },
-                    onDragEnd: (){
-                      print('绘制完成了！');
-                      return true;
-                    },
-                    center: LatLng(51.5, -0.09),
-                    zoom: 5.0,
-                    maxZoom: 5.0,
-                    minZoom: 3.0,
-                  ),
-                  layers: [
-                    TileLayerOptions(
-                        urlTemplate:
-                            'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                        subdomains: ['a', 'b', 'c']),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: MyMap()
     );
   }
 }
 
+class MyMap extends StatefulWidget {
+  @override
+  _MyMapState createState() => _MyMapState();
+}
 
+class _MyMapState extends State<MyMap> {
+
+  List<LatLng> _points;
+
+  @override
+  void initState() {
+    _points = <LatLng>[
+      LatLng(51.5, -0.09),
+      LatLng(53.3498, -6.2603),
+      LatLng(48.8566, 2.3522),
+    ];
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              child: FlutterMap(
+                options: MapOptions(
+                  onTap: (lat){
+                    setState(() {
+                      _points.add(lat); 
+                    });
+                  },
+                  onDragStart: (lat){
+                    return true;
+                  },
+                  onDragUpdate: (lat){
+                    setState(() {
+                    _points.add(lat); 
+                    });
+                  },
+                  onDragEnd: (){
+                    return true;
+                  },
+                  center: LatLng(51.5, -0.09),
+                  zoom: 5.0,
+                  maxZoom: 5.0,
+                  minZoom: 3.0,
+                ),
+                layers: [
+                  TileLayerOptions(
+                      urlTemplate:
+                          'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                      subdomains: ['a', 'b', 'c']),
+                  PolylineLayerOptions(
+                  polylines: [
+                    Polyline(
+                        points: _points,
+                        strokeWidth: 4.0,
+                        color: Colors.purple),
+                  ],
+                )   
+                ],
+              )
+            )
+          ],
+        )
+      );
+  }
+}
